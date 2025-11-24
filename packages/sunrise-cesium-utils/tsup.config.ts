8e6a540e287fe17709670c8c6daf4371bae12425
朱重迁 <z32664206862022@163.com>
2025-11-24 15:06:36 +0800

    feat: 新增 sunrise-cesium-utils 库及相关功能，更新文档和示例

--- packages/sunrise-cesium-utils/tsup.config.ts
+++ packages/sunrise-cesium-utils/tsup.config.ts
@@ -3,16 +3,21 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   dts: true,
-  format: ["iife"], // 使用IIFE格式，确保所有代码被打包进一个文件
+  format: ["iife", "esm"], // 使用IIFE格式，确保所有代码被打包进一个文件
   outDir: "dist",
   minify: true,
   bundle: true,
   splitting: false,
   globalName: "SunriseCesiumUtils",
   injectStyle: true,
-  outExtension({ format, options }) {
-    return {
-      js: options.minify ? ".min.js" : `.js`,
-    };
+  // 为不同格式设置不同的输出文件名
+  outExtension({ format }) {
+    if (format === "iife") {
+      return "[name].global";
+    } else if (format === "esm") {
+      return "[name].mjs";
+    }
+    return "[name]";
   },
+  legacyOutput: true,
 });
